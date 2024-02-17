1f64e8ffffe8881895f71e5635e15ede80ce5546
Tomasz <trnosal@gmail.com>
2024-02-17 16:26:40 +0100

    Add DataContext

---
+++ src/Jared.Infrastructure/Persistence/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using Jared.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jared.Infrastructure.Persistence;
+
+public class DataContext : DbContext
+{
+    public DataContext()
+    {
+    }
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Epic>? Epics { get; set; }
+    public DbSet<User>? Users { get; set; }
+    public DbSet<History>? History { get; set; }
+    public DbSet<Domain.Models.Task>? Tasks { get; set; }
+    public DbSet<Project>? Projects { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+    }
+}
