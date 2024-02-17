b0c4fec551c561e9735bb5cdf305fd384448e1bc
Tomasz <trnosal@gmail.com>
2024-02-17 16:27:04 +0100

    Add model configuration

---
+++ src/Jared.Infrastructure/ModelConfiguration/EpicConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Jared.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Jared.Infrastructure.ModelConfiguration;
+
+public class EpicConfiguration : IEntityTypeConfiguration<Epic>
+{
+    public void Configure(EntityTypeBuilder<Epic> builder)
+    {
+        builder.HasOne(e => e.Project)
+            .WithMany(p => p.Epics)
+            .HasForeignKey(e => e.ProjectId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
