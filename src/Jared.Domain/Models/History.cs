5aceb2fef7ea753754fe27ce3d2f1e438ed5a41d
Tomasz <trnosal@gmail.com>
2024-02-24 08:50:32 +0100

    Remove User and History entities

--- src/Jared.Domain/Models/History.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Jared.Domain.Models;
-
-public class History
-{
-    public int Id { get; set; }
-    public DateTime Time { get; set; }
-}
--- src/Jared.Domain/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Jared.Domain.Models;
-
-public class User
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = default!;
-    public string Surname { get; set; } = default!;
-}
