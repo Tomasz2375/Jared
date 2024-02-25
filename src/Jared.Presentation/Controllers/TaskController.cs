374fe1870601811ce18417a02e14c704ac86ddaf
Tomasz <trnosal@gmail.com>
2024-02-25 10:41:48 +0100

    Add TaskController

---
+++ src/Jared.Presentation/Controllers/TaskController.cs
@@ -0,0 +1,26 @@
+﻿using Jared.Application.Commands.Task;
+using Jared.Domain.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jared.Presentation.Controllers;
+
+[ApiController]
+[Route("controller")]
+public class TaskController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public TaskController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<Result> CreateTask(CreateTaskCommand command)
+    {
+        await mediator.Send(command);
+
+        return Result.Ok();
+    }
+}
