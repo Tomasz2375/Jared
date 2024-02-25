ff42e8b22621ebe0445ca0839db2733d7b341f1b
Tomasz <trnosal@gmail.com>
2024-02-25 10:39:01 +0100

    Create abstraction

---
+++ src/Jared.Domain/Abstractions/Response.cs
@@ -0,0 +1,40 @@
+﻿using Jared.Domain.Enums;
+
+namespace Jared.Domain.Abstractions;
+
+public class Response<T>
+{
+    public const int SUCCESS_CODE = 200;
+    public const int GENERAL_ERROR_CODE = 500;
+
+    public Response()
+        : this(null, SUCCESS_CODE, default)
+    {
+    }
+
+    public Response(T data)
+        : this(null, SUCCESS_CODE, data)
+    {
+    }
+
+    public Response(ErrorModel error, T? data = default)
+        : this(error, GENERAL_ERROR_CODE, data)
+    {
+    }
+
+    public Response(ErrorCode code, ErrorModel? error = null)
+        : this(error, (int)code, default)
+    {
+    }
+
+    private Response(ErrorModel? error, int code, T? data)
+    {
+        Error = error;
+        Code = code;
+        Data = data;
+    }
+
+    public ErrorModel? Error { get; set; }
+    public int Code { get; set; }
+    public T? Data { get; set; }
+}
---
+++ src/Jared.Domain/Abstractions/Result.cs
@@ -0,0 +1,51 @@
+﻿namespace Jared.Domain.Abstractions;
+
+public class Result
+{
+    public bool Success { get; set; }
+    public string Error { get; private set; } = default!;
+    
+    protected Result(bool success, string error)
+    {
+        if (success && error != string.Empty)
+        {
+            throw new InvalidOperationException();
+        }
+        if (!success && error == string.Empty)
+        {
+            throw new InvalidOperationException();
+        }
+
+        Success = success;
+        Error = error;
+    }
+
+    public static Result Fail(string message)
+    {
+        return new Result(false, message);
+    }
+    public static Result<T> Fail<T>(string message)
+    {
+        return new Result<T>(default(T)!, false, message);
+    }
+    public static Result Ok()
+    {
+        return new Result(true, string.Empty);
+    }
+    public static Result<T> Ok<T>(T value)
+    {
+        return new Result<T>(value, true, string.Empty);
+    }
+
+}
+
+public class Result<T> : Result
+{
+    public T Value { get; set; }
+
+    protected internal Result(T value, bool success, string error)
+        : base(success, error)
+    {
+        Value = value;
+    }
+}
