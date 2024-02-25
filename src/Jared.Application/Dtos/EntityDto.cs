7ad3751e4a7905cff14c662347150ea95e4b711f
Tomasz <trnosal@gmail.com>
2024-02-25 10:40:16 +0100

    Add EntityDto

---
+++ src/Jared.Application/Dtos/EntityDto.cs
@@ -0,0 +1,24 @@
+﻿using Jared.Domain.Interfaces;
+
+namespace Jared.Application.Dtos;
+
+public abstract class EntityDto<TKey> : IEntity<TKey>
+{
+    public TKey Id { get; set; } = default!;
+
+    object IEntity.Id
+    {
+        get
+        {
+            return Id!;
+        }
+        set
+        {
+            if (value is not TKey key)
+            {
+                throw new ArgumentException("Wrong entity Id type");
+            }
+            Id = key;
+        }
+    }
+}
