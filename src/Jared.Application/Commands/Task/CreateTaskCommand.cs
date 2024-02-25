f0524608efa3365c34a3431627972687bc4d1569
Tomasz <trnosal@gmail.com>
2024-02-25 10:41:38 +0100

    Add CreateTaskCommand

---
+++ src/Jared.Application/Commands/Task/CreateTaskCommand.cs
@@ -0,0 +1,8 @@
+﻿using Jared.Application.Dtos.TaskDto;
+using Jared.Domain.Abstractions;
+using Jared.Domain.Interfaces;
+using MediatR;
+
+namespace Jared.Application.Commands.Task;
+
+public sealed record CreateTaskCommand(TaskDetailsDto dto) : IRequest<Result>;
---
+++ src/Jared.Application/Commands/Task/CreateTaskCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Jared.Domain.Abstractions;
+using Jared.Domain.Interfaces;
+using MediatR;
+using TaskE = Jared.Domain.Models.Task;
+
+namespace Jared.Application.Commands.Task;
+
+public class CreateTaskCommandHandler : IRequestHandler<CreateTaskCommand, Result>
+{
+    private readonly IDataContext dataContext;
+
+    public CreateTaskCommandHandler(IDataContext dataContext)
+    {
+        this.dataContext = dataContext;
+    }
+
+    public async Task<Result> Handle(CreateTaskCommand command, CancellationToken cancellationToken)
+    {
+        TaskE task = new()
+        {
+            Title = command.dto.Title,
+            Description = command.dto.Description,
+            Deadline = command.dto.Deadline,
+            StartDate = command.dto.StartDate,
+            EndDate = command.dto.EndDate,
+        };
+
+        try
+        {
+            dataContext.Add(task);
+            await dataContext.SaveChangesAsync(cancellationToken);
+            
+            return Result.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Result.Fail(ex.Message);
+        }
+
+    }
+}
