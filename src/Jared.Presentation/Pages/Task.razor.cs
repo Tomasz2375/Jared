713d95e5fe4785dbe8f6d70b8f3c0c058bd310b4
Tomasz <trnosal@gmail.com>
2024-03-02 16:04:35 +0100

    Add Task page

---
+++ src/Jared.Presentation/Pages/Task.razor.cs
@@ -0,0 +1,22 @@
+﻿using Jared.Application.Dtos.TaskDto;
+using Jared.Application.Queries.TaskQueries;
+using Microsoft.AspNetCore.Components;
+
+namespace Jared.Presentation.Pages;
+
+public partial class Task : ComponentBase
+{
+    public List<TaskListDto> Model { get; set; } = new();
+
+    protected override async System.Threading.Tasks.Task OnInitializedAsync()
+    {
+        var result = await Mediator.Send(new TaskListQuery());
+
+        if (!result.Success)
+        {
+            return;
+        }
+
+        Model = result.Data;
+    }
+}
