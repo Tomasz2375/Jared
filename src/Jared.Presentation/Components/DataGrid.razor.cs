39fd59cf08ce25640f3dbd330f5d92ac15b76679
Tomasz <trnosal@gmail.com>
2024-03-02 17:07:06 +0100

    Create DateGrid component

---
+++ src/Jared.Presentation/Components/DataGrid.razor.cs
@@ -0,0 +1,10 @@
+﻿using Jared.Application.Dtos.TaskDto;
+using Microsoft.AspNetCore.Components;
+
+namespace Jared.Presentation.Components;
+
+public partial class DataGrid<TItem>
+{
+    [Parameter]
+    public List<TItem> Items { get; set; } = new();
+}
