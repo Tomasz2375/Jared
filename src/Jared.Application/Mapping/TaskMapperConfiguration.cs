ccfba2b56bb759841d23b3569b0e076d1c8086bf
Tomasz <trnosal@gmail.com>
2024-03-03 11:59:13 +0100

    Remove unnecessary TaskMapperConfiguration

--- src/Jared.Application/Mapping/TaskMapperConfiguration.cs
+++ src/Jared.Application/Mapping/TaskMapperConfiguration.cs
@@ -1,5 +1,4 @@
-﻿using Jared.Application.Dtos.TaskDto;
-using Mapster;
+﻿using Mapster;
 
 namespace Jared.Application.Mapping;
 
@@ -7,19 +6,5 @@ public class TaskMapperConfiguration : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
-        config
-            .NewConfig<Domain.Models.Task, TaskListDto>()
-            .IgnoreNullValues(true);
-        config
-            .NewConfig<TaskListDto, Domain.Models.Task>()
-            .IgnoreNullValues(true);
-
-        config
-            .NewConfig<Domain.Models.Task, TaskDetailsDto>()
-            .IgnoreNullValues(true);
-        config
-            .NewConfig<TaskDetailsDto, Domain.Models.Task>()
-            .IgnoreNullValues(true);
-
     }
 }
