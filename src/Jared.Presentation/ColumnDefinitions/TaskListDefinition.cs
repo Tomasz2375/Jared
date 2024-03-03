42b548b83d8220b8f2f1f4c65109ee756c5aabd6
Tomasz <trnosal@gmail.com>
2024-03-03 12:01:20 +0100

    Create TaskListDefinition

---
+++ src/Jared.Presentation/ColumnDefinitions/TaskListDefinition.cs
@@ -0,0 +1,31 @@
+﻿using Jared.Application.Dtos.TaskDto;
+using Jared.Presentation.ColumnDefinitions.Abstraction;
+
+namespace Jared.Presentation.ColumnDefinitions;
+
+public class TaskListDefinition : ListDefinitionBase<TaskListDto>
+{
+    public IColumnDefinition<TaskListDto> Id { get; } =
+        new ColumnDefinition<TaskListDto>("Id", x => x.Id);
+
+    public IColumnDefinition<TaskListDto> Title { get; } =
+        new ColumnDefinition<TaskListDto>("Title", x => x.Title);
+
+    public IColumnDefinition<TaskListDto> Code { get; } =
+        new ColumnDefinition<TaskListDto>("Code", x => x.Code);
+
+    public IColumnDefinition<TaskListDto> Priority { get; } =
+        new ColumnDefinition<TaskListDto>("Priority", x => x.Priority);
+
+    public IColumnDefinition<TaskListDto> Status { get; } =
+        new ColumnDefinition<TaskListDto>("Status", x => x.Status);
+
+    public override IEnumerable<IColumnDefinition<TaskListDto>> GetColumns()
+    {
+        yield return Id;
+        yield return Title;
+        yield return Code;
+        yield return Priority;
+        yield return Status;
+    }
+}
