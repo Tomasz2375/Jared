a4294a6cbea2ca4da677bba14d3593dfea7e86cf
Tomasz <trnosal@gmail.com>
2024-03-03 12:00:00 +0100

    Create list definition abstraction

---
+++ src/Jared.Presentation/ColumnDefinitions/Abstraction/IListDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace Jared.Presentation.ColumnDefinitions.Abstraction;
+
+public interface IListDefinition<T> : IListDefinition
+{
+    new IEnumerable<IColumnDefinition<T>> GetColumns();
+}
+
+public interface IListDefinition
+{
+    IEnumerable<IColumnDefinition> GetColumns();
+}
---
+++ src/Jared.Presentation/ColumnDefinitions/Abstraction/ListDefinitionBase.cs
@@ -0,0 +1,11 @@
+﻿namespace Jared.Presentation.ColumnDefinitions.Abstraction;
+
+public abstract class ListDefinitionBase<T> : IListDefinition<T>
+{
+    public abstract IEnumerable<IColumnDefinition<T>> GetColumns();
+
+    IEnumerable<IColumnDefinition> IListDefinition.GetColumns()
+    {
+        return GetColumns();
+    }
+}
