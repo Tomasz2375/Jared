fb8a26bf64f0db6a1842b2d0466cf7b3ea97eb09
Tomasz <trnosal@gmail.com>
2024-03-03 11:58:52 +0100

    Modify TaskRootDto

--- src/Jared.Application/Dtos/TaskDto/TaskRootDto.cs
+++ src/Jared.Application/Dtos/TaskDto/TaskRootDto.cs
@@ -1,10 +1,13 @@
-﻿namespace Jared.Application.Dtos.TaskDto
+﻿using Jared.Domain.Enums;
+using TaskStatus = Jared.Domain.Enums.TaskStatus;
+
+namespace Jared.Application.Dtos.TaskDto
 {
     public class TaskRootDto : EntityDto<int>
     {
         public string Title { get; set; } = default!;
         public string? Code { get; set; }
-        public int StatusId { get; set; }
-        public int PriorityId { get; set; }
+        public TaskStatus Status { get; set; }
+        public Priority Priority { get; set; }
     }
 }
