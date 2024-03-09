d39b76070218358c302c2ff409295985714f6eeb
Tomasz <trnosal@gmail.com>
2024-03-09 13:26:54 +0100

    Add TaskPageDto

---
+++ src/Jared.Application/Dtos/TaskDto/TaskPageDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Jared.Application.Dtos.TaskDto;
+
+public class TaskPageDto
+{
+    public int TasksCount { get; set; }
+    public int TasksFrom { get; set; }
+    public int TasksTo { get; set; }
+    public int? CurrentPage { get; set; }
+    public int? PageSize { get; set; }
+    public int? PageCount { get; set; }
+    public List<TaskListDto> Tasks { get; set; } = new();
+}
