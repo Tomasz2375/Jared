b79402b6b20df19258f82f72bb9f05728bd439ea
Tomasz <trnosal@gmail.com>
2024-03-10 12:20:43 +0100

    Add pagination model

---
+++ src/Jared.Application/Dtos/Abstractions/IPagination.cs
@@ -0,0 +1,11 @@
+﻿namespace Jared.Application.Dtos.Abstractions;
+
+public interface IPagination
+{
+    public int ItemsCount { get; set; }
+    public int ItemFrom { get; set; }
+    public int ItemTo { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+}
---
+++ src/Jared.Application/Dtos/PageDto/PaginationDto.cs
@@ -0,0 +1,13 @@
+﻿using Jared.Application.Dtos.Abstractions;
+
+namespace Jared.Application.Dtos.PageDto;
+
+public class PaginationDto : IPagination
+{
+    public int ItemsCount { get; set; }
+    public int ItemFrom { get; set; }
+    public int ItemTo { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public int PageCount { get; set; }
+}
