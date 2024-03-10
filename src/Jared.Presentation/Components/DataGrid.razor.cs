0572c5471615c25369f9be363e1e4065fa852ae8
Tomasz <trnosal@gmail.com>
2024-03-10 12:22:20 +0100

    Update DataGrid component

--- src/Jared.Presentation/Components/DataGrid.razor.cs
+++ src/Jared.Presentation/Components/DataGrid.razor.cs
@@ -1,4 +1,5 @@
-﻿using Jared.Application.Dtos.TaskDto;
+﻿using Jared.Application.Dtos.Abstractions;
+using Jared.Presentation.ColumnDefinitions;
 using Jared.Presentation.ColumnDefinitions.Abstraction;
 using Microsoft.AspNetCore.Components;
 
@@ -10,4 +11,32 @@ public partial class DataGrid<TItem>
     public List<TItem> Items { get; set; } = new();
     [Parameter]
     public IEnumerable<IColumnDefinition<TItem>> ItemsDefinition { get; set; }
+    [Parameter]
+    public IPagination Pagination { get; set; }
+    [Parameter]
+    public Query Query { get; set; }
+    [Parameter]
+    public EventCallback<Query> SendPageQuery { get; set; }
+
+    private void previousPage()
+    {
+        if (Query.Page == 1)
+        {
+            return;
+        }
+
+        Query.Page--;
+        SendPageQuery.InvokeAsync(Query);
+    }
+    private void nextPage()
+    {
+        if (Query.Page == Pagination.PageCount)
+        {
+            return;
+        }
+
+        Query.Page++;
+        SendPageQuery.InvokeAsync(Query);
+    }
+
 }
