a3bbc44dd633ff9d9609691a4aa9d09aac61f743
Tomasz <trnosal@gmail.com>
2024-03-10 12:21:52 +0100

    Add Query model

---
+++ src/Jared.Presentation/ColumnDefinitions/Abstraction/IQuery.cs
@@ -0,0 +1,12 @@
+﻿using Jared.Domain.Enums;
+
+namespace Jared.Presentation.ColumnDefinitions.Abstraction;
+
+public interface IQuery
+{
+    public int Page {  get; set; }
+    public int PageSize { get; set; }
+    public string? Filter { get; set; }
+    public string? SortingProperty { get; set; }
+    public SortingDirection? SortingDirection { get; set; }
+}
---
+++ src/Jared.Presentation/ColumnDefinitions/Query.cs
@@ -0,0 +1,14 @@
+﻿using Jared.Domain.Enums;
+using Jared.Presentation.ColumnDefinitions.Abstraction;
+
+namespace Jared.Presentation.ColumnDefinitions
+{
+    public class Query : IQuery
+    {
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+        public string? Filter { get; set; }
+        public string? SortingProperty { get; set; }
+        public SortingDirection? SortingDirection { get; set; }
+    }
+}
