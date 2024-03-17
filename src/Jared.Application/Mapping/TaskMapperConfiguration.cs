7f018b9c85c614f0f51333e83f61477a21e86c80
Tomasz <trnosal@gmail.com>
2024-03-17 13:36:30 +0100

    Update TaskMapperConfiguration

--- src/Jared.Application/Mapping/TaskMapperConfiguration.cs
+++ src/Jared.Application/Mapping/TaskMapperConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using Mapster;
+﻿using Jared.Application.Dtos.TaskDto;
+using Mapster;
+using Task = Jared.Domain.Models.Task;
 
 namespace Jared.Application.Mapping;
 
@@ -6,5 +8,32 @@ public class TaskMapperConfiguration : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
+        config.NewConfig<TaskRootDto, Task>()
+            .Map(
+                d => d.PriorityId,
+                s => s.Priority)
+            .Map(
+                d => d.StatusId,
+                s => s.Status);
+
+        config.NewConfig<Task, TaskRootDto>()
+            .Map(
+                d => d.Status,
+                s => s.StatusId)
+            .Map(
+                d => d.Priority,
+                s => s.PriorityId);
+
+        config.NewConfig<Task, TaskListDto>()
+            .Inherits<Task, TaskRootDto>();
+
+        config.NewConfig<TaskListDto, Task>()
+            .Inherits<TaskRootDto, Task>();
+
+        config.NewConfig<Task, TaskDetailsDto>()
+            .Inherits<Task, TaskRootDto>();
+
+        config.NewConfig<TaskDetailsDto, Task>()
+            .Inherits<TaskRootDto, Task>();
     }
 }
