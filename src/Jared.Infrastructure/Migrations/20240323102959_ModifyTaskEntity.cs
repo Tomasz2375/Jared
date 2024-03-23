d4a6c97f4668a489f1e656e09b061bcd1090d5b6
Tomasz <trnosal@gmail.com>
2024-03-23 11:33:41 +0100

    Create migration rename enum values

---
+++ src/Jared.Infrastructure/Migrations/20240323102959_ModifyTaskEntity.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jared.Infrastructure.Migrations
+{
+    public partial class ModifyTaskEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StatusId",
+                table: "Task",
+                newName: "Status");
+
+            migrationBuilder.RenameColumn(
+                name: "PriorityId",
+                table: "Task",
+                newName: "Priority");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Task",
+                newName: "StatusId");
+
+            migrationBuilder.RenameColumn(
+                name: "Priority",
+                table: "Task",
+                newName: "PriorityId");
+        }
+    }
+}
