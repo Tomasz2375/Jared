0252e48f67f4e761b6de756e9f69440b081c9dc2
Tomasz <trnosal@gmail.com>
2024-03-23 11:10:32 +0100

    Create InputTextField component

---
+++ src/Jared.Presentation/Components/Basic/InputTextField.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+
+namespace Jared.Presentation.Components.Basic
+{
+    public partial class InputTextField
+    {
+        [Parameter]
+        public Expression<Func<string>> ValidationFor { get; set; } = default!;
+        [Parameter]
+        public string? Id { get; set; }
+        [Parameter]
+        public string? Label { get; set; }
+
+        protected override bool TryParseValueFromString(string? value, out string result, out string validationErrorMessage)
+        {
+            result = value!;
+            validationErrorMessage = null!;
+            return true;
+        }
+    }
+}
