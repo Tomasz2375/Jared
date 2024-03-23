f712466fbf0304ad7154e504862133a524b18b13
Tomasz <trnosal@gmail.com>
2024-03-23 11:33:23 +0100

    Modify Task entity

--- src/Jared.Domain/Models/Task.cs
+++ src/Jared.Domain/Models/Task.cs
@@ -1,4 +1,5 @@
 ﻿using Jared.Domain.Abstractions;
+using Jared.Domain.Enums;
 
 namespace Jared.Domain.Models;
 
@@ -7,12 +8,12 @@ public class Task : Entity
     public int? ParentId { get; set; }
     public int? ProjectId { get; set; }
     public int? EpicId { get; set; }
-    public int StatusId { get; set; }
-    public int PriorityId { get; set; }
 
     public string Title { get; set; } = default!;
     public string? Description { get; set; }
     public string? Code { get; set; } = default!;
+    public Enums.TaskStatus Status { get; set; }
+    public Priority Priority { get; set; }
     public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? StartDate { get; set; }
     public DateTime? EndDate { get; set; }
