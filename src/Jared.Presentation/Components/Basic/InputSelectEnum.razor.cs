c2a4481ed07e31d4f0efa26324c9bc652c80886a
Tomasz <trnosal@gmail.com>
2024-03-23 14:37:27 +0100

    Create InputSelectEnum component

---
+++ src/Jared.Presentation/Components/Basic/InputSelectEnum.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Linq.Expressions;
+
+namespace Jared.Presentation.Components.Basic;
+
+public partial class InputSelectEnum<TEnum> where TEnum : struct, Enum
+{
+    [Parameter]
+    public Expression<Func<TEnum>> ValidationFor { get; set; } = default!;
+    [Parameter]
+    public string? Id { get; set; }
+    [Parameter]
+    public string? Label { get; set; }
+    [Parameter]
+    public EventCallback<TEnum> EnumPropertyChanged { get; set; }
+
+    protected override bool TryParseValueFromString(string? value, out TEnum result, out string validationErrorMessage)
+    {
+        result = (TEnum)Enum.Parse(typeof(TEnum).GetType(), value!);
+        validationErrorMessage = null!;
+        return true;
+    }
+
+}
