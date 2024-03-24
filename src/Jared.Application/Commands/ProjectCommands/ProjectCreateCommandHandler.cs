9c7d966005c914ec5e3bea566e544203d95f3dc4
Tomasz <trnosal@gmail.com>
2024-03-24 12:14:57 +0100

    Add ProjectCreate command

---
+++ src/Jared.Application/Commands/ProjectCommands/ProjectCreateCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using Jared.Application.Commands.ProjectCommands;
+using Jared.Domain.Abstractions;
+using Jared.Domain.Interfaces;
+using Jared.Domain.Models;
+using MapsterMapper;
+using MediatR;
+
+namespace Jared.Application.Commands.TaskCommand;
+
+public class ProjectCreateCommandHandler : IRequestHandler<ProjectCreateCommand, Result>
+{
+    private readonly IDataContext dataContext;
+    private readonly IMapper mapper;
+
+    public ProjectCreateCommandHandler(IDataContext dataContext, IMapper mapper)
+    {
+        this.dataContext = dataContext;
+        this.mapper = mapper;
+    }
+
+#pragma warning disable CS1998
+    public async Task<Result> Handle(ProjectCreateCommand command, CancellationToken cancellationToken)
+#pragma warning restore CS1998
+    {
+        var project = mapper.Map<Project>(command.dto);
+
+        try
+        {
+            dataContext.Add(project);
+            await dataContext.SaveChangesAsync(cancellationToken);
+
+            return Result.Ok();
+        }
+        catch (Exception ex)
+        {
+            return Result.Fail(ex.Message);
+        }
+    }
+}
