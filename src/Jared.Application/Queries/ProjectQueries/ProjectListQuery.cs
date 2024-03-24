a38b3be25bfc0a8a92380e8477a54cb8f159bd05
Tomasz <trnosal@gmail.com>
2024-03-24 12:15:29 +0100

    Add project list query

---
+++ src/Jared.Application/Queries/ProjectQueries/ProjectListQuery.cs
@@ -0,0 +1,8 @@
+﻿using Jared.Application.Dtos.ProjectDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Application.Queries.ProjectQueries
+{
+    public sealed record ProjectListQuery() : IRequest<Result<List<ProjectListDto>>>;
+}
---
+++ src/Jared.Application/Queries/ProjectQueries/ProjectListQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using Jared.Application.Dtos.ProjectDtos;
+using Jared.Domain.Abstractions;
+using Jared.Domain.Interfaces;
+using Jared.Domain.Models;
+using MapsterMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jared.Application.Queries.ProjectQueries;
+
+public class ProjectListQueryHandler : IRequestHandler<ProjectListQuery, Result<List<ProjectListDto>>>
+{
+    private readonly IDataContext dataContext;
+    private readonly IMapper mapper;
+
+    public ProjectListQueryHandler(IDataContext dataContext, IMapper mapper)
+    {
+        this.dataContext = dataContext;
+        this.mapper = mapper;
+    }
+
+#pragma warning disable CS1998
+    public async Task<Result<List<ProjectListDto>>> Handle(ProjectListQuery request, CancellationToken cancellationToken)
+#pragma warning disable CS1998
+    {
+        var projectsQuery = dataContext
+            .Set<Project>()
+            .AsNoTracking();
+
+        var projects = projectsQuery.AsEnumerable();
+
+        var result = mapper.Map<List<ProjectListDto>>(projects);
+
+        return Result.Ok(result);
+    }
+}
