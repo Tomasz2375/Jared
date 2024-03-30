026f10c9adaad61f4c84d00aef8df85da14c573a
Tomasz <trnosal@gmail.com>
2024-03-30 11:22:34 +0100

    Add TaskCreateForm

---
+++ src/Jared.Presentation/Components/Forms/TaskCreateForm.razor.cs
@@ -0,0 +1,40 @@
+﻿using Jared.Application.Commands.TaskCommand;
+using Jared.Application.Dtos.TaskDtos;
+using Microsoft.AspNetCore.Components;
+
+namespace Jared.Presentation.Components.Forms;
+
+public partial class TaskCreateForm
+{
+    [Parameter]
+    public EventCallback CloseDialog { get; set; }
+
+    [Parameter]
+    public string? Title { get; set; }
+
+    public TaskDetailsDto Dto { get; set; } = new();
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        Dto.Deadline = DateTime.Now.Date;
+        Dto.Priority = Domain.Enums.Priority.Normal;
+        Dto.Status = Domain.Enums.TaskStatus.ToDo;
+    }
+
+    private void cancel()
+    {
+        CloseDialog.InvokeAsync();
+    }
+
+    private async Task save()
+    {
+        var result = await Mediator.Send(new TaskCreateCommand(Dto));
+        if (!result.Success)
+        {
+            Console.WriteLine("Create task failed");
+        }
+
+        await CloseDialog.InvokeAsync();
+    }
+}
