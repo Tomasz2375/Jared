5226c79078bfe3d85c7f4ad2b30d4b80365efd2b
Tomasz <trnosal@gmail.com>
2024-03-30 11:19:36 +0100

    Add ProjectPageDto

---
+++ src/Jared.Application/Dtos/ProjectDtos/ProjectPageDto.cs
@@ -0,0 +1,9 @@
+﻿using Jared.Application.Dtos.PageDtos;
+
+namespace Jared.Application.Dtos.ProjectDtos;
+
+public class ProjectPageDto
+{
+    public PaginationDto Pagination { get; set; } = default!;
+    public IEnumerable<ProjectListDto> Projects { get; set; } = Array.Empty<ProjectListDto>();
+}
