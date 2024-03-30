b2e65d12f79645c4084c1c68f9ca0e53bd000e0b
Tomasz <trnosal@gmail.com>
2024-03-30 12:31:31 +0100

    Create EpicListDefinition

---
+++ src/Jared.Presentation/ColumnDefinitions/EpicListDefinition.cs
@@ -0,0 +1,28 @@
+﻿using Jared.Application.Dtos.EpicDtos;
+using Jared.Domain.Enums;
+using Jared.Presentation.ColumnDefinitions.Abstraction;
+
+namespace Jared.Presentation.ColumnDefinitions;
+
+public class EpicListDefinition : ListDefinitionBase<EpicListDto>
+{
+    public IColumnDefinition<EpicListDto> Id { get; } =
+        new ColumnDefinition<EpicListDto>("Id", x => x.Id, ColumnAlignment.Left);
+
+    public IColumnDefinition<EpicListDto> Title { get; } =
+        new ColumnDefinition<EpicListDto>("Title", x => x.Title, ColumnAlignment.Left);
+
+    public IColumnDefinition<EpicListDto> ParentId { get; } =
+        new ColumnDefinition<EpicListDto>("ParentId", x => x.ParentId, ColumnAlignment.Left);
+
+    public IColumnDefinition<EpicListDto> ProjectId { get; } =
+        new ColumnDefinition<EpicListDto>("ProjectId", x => x.ProjectId, ColumnAlignment.Left);
+
+    public override IEnumerable<IColumnDefinition<EpicListDto>> GetColumns()
+    {
+        yield return Id;
+        yield return Title;
+        yield return ParentId;
+        yield return ProjectId;
+    }
+}
