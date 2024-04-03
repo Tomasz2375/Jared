7970754bcf4a4dc3adb3eaf3fce2b45a1dfbc004
Tomasz <trnosal@gmail.com>
2024-04-03 20:46:38 +0200

    Create EpicListQuery

---
+++ src/Jared.Application/Queries/EpicQueries/EpicListQuery.cs
@@ -0,0 +1,7 @@
+﻿using Jared.Application.Dtos.EpicDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Application.Queries.EpicQueries;
+
+public record EpicListQuery(int? projectId) : IRequest<Result<List<EpicListDto>>>;
