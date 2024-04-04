cd8d4f88e461c0b068bf425e30a951193f379277
Tomasz <trnosal@gmail.com>
2024-04-04 21:54:09 +0200

    Create TaskDetailsQuery

---
+++ src/Jared.Application/Queries/TaskQueries/TaskDetailsQuery.cs
@@ -0,0 +1,9 @@
+﻿using Jared.Application.Dtos.TaskDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Application.Queries.TaskQueries;
+
+public record TaskDetailsQuery(int id) : IRequest<Result<TaskDetailsDto>>
+{
+}
