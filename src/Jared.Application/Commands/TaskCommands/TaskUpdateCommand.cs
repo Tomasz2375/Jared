1f9f5e267352bc4d3173a6cee78748e56d72b701
Tomasz <trnosal@gmail.com>
2024-04-05 22:34:26 +0200

    Create TaskUpdateCommand

---
+++ src/Jared.Application/Commands/TaskCommands/TaskUpdateCommand.cs
@@ -0,0 +1,7 @@
+﻿using Jared.Application.Dtos.TaskDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Application.Commands.TaskCommands;
+
+public record TaskUpdateCommand(TaskDetailsDto dto) : IRequest<Result<TaskDetailsDto>>;
