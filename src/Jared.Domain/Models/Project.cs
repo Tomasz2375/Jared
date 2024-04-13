cdfa6d58299d2397d5d0395755520b30bf9ecaa5
Tomasz <trnosal@gmail.com>
2024-04-13 12:53:11 +0200

    Create migration AddLastTaskAndEpicToProjectEntity

--- src/Jared.Domain/Models/Project.cs
+++ src/Jared.Domain/Models/Project.cs
@@ -8,6 +8,9 @@ public class Project : Entity
     public string? Description { get; set; }
     public string Code { get; set; } = default!;
 
+    public int LastTaskNumber { get; set; }
+    public int LastEpicNumber { get; set; }
+
     public List<Task> Tasks { get; set; } = new();
     public List<Epic> Epics { get; set; } = new();
 }
