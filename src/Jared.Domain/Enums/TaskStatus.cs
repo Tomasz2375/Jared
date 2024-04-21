c64f8ffca633a4e7f053356d561d4acfb6e42356
Tomasz <trnosal@gmail.com>
2024-04-21 22:28:59 +0200

    Fix TaskStatuses

--- src/Jared.Domain/Enums/TaskStatus.cs
+++ src/Jared.Domain/Enums/TaskStatus.cs
@@ -1,11 +1,13 @@
 ﻿namespace Jared.Domain.Enums;
 
+[Flags]
 public enum TaskStatus
 {
-    Created,
-    ToDo,
-    Doing,
-    Done,
-    Blocked,
-    Canceled,
+    None = 0,
+    Created = 1,
+    ToDo = 2,
+    Doing = 4,
+    Done = 8,
+    Blocked = 16,
+    Canceled = 32,
 }
