8aa02ef10c90158191183b97a310d5c17341f875
Tomasz <trnosal@gmail.com>
2024-04-25 19:53:38 +0200

    Update definitions for entities

--- src/Jared.Presentation/ColumnDefinitions/EpicListDefinition.cs
+++ src/Jared.Presentation/ColumnDefinitions/EpicListDefinition.cs
@@ -7,16 +7,16 @@ namespace Jared.Presentation.ColumnDefinitions;
 public class EpicListDefinition : ListDefinitionBase<EpicListDto>
 {
     public IColumnDefinition<EpicListDto> Id { get; } =
-        new ColumnDefinition<EpicListDto>("Id", x => x.Id, ColumnAlignment.Left, "1");
+        new ColumnDefinition<EpicListDto>("Id", x => x.Id, ColumnAlignment.Left, "1", null);
 
     public IColumnDefinition<EpicListDto> Title { get; } =
-        new ColumnDefinition<EpicListDto>("Title", x => x.Title, ColumnAlignment.Left, "4");
+        new ColumnDefinition<EpicListDto>("Title", x => x.Title, ColumnAlignment.Left, "4", null);
 
     public IColumnDefinition<EpicListDto> ParentId { get; } =
-        new ColumnDefinition<EpicListDto>("ParentId", x => x.ParentId, ColumnAlignment.Left, "1");
+        new ColumnDefinition<EpicListDto>("ParentId", x => x.ParentId, ColumnAlignment.Left, "1", null);
 
     public IColumnDefinition<EpicListDto> ProjectId { get; } =
-        new ColumnDefinition<EpicListDto>("ProjectId", x => x.ProjectId, ColumnAlignment.Left, "1");
+        new ColumnDefinition<EpicListDto>("ProjectId", x => x.ProjectId, ColumnAlignment.Left, "1", null);
 
     public override IEnumerable<IColumnDefinition<EpicListDto>> GetColumns()
     {
