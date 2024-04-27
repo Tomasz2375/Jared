e6eb0158ae8f10853e67172052b3597d06707ebe
Tomasz <trnosal@gmail.com>
2024-04-27 13:19:12 +0200

    Update definitions for pages

--- src/Jared.Presentation/ColumnDefinitions/EpicListDefinition.cs
+++ src/Jared.Presentation/ColumnDefinitions/EpicListDefinition.cs
@@ -7,16 +7,16 @@ namespace Jared.Presentation.ColumnDefinitions;
 public class EpicListDefinition : ListDefinitionBase<EpicListDto>
 {
     public IColumnDefinition<EpicListDto> Id { get; } =
-        new ColumnDefinition<EpicListDto>("Id", x => x.Id, ColumnAlignment.Left, "1", null);
+        new ColumnDefinition<EpicListDto>("Id", typeof(int), x => x.Id, ColumnAlignment.Left, "1", null);
 
     public IColumnDefinition<EpicListDto> Title { get; } =
-        new ColumnDefinition<EpicListDto>("Title", x => x.Title, ColumnAlignment.Left, "4", null);
+        new ColumnDefinition<EpicListDto>("Title", typeof(string), x => x.Title, ColumnAlignment.Left, "4", null);
 
     public IColumnDefinition<EpicListDto> ParentId { get; } =
-        new ColumnDefinition<EpicListDto>("ParentId", x => x.ParentId, ColumnAlignment.Left, "1", null);
+        new ColumnDefinition<EpicListDto>("ParentId", typeof(int?), x => x.ParentId, ColumnAlignment.Left, "1", null);
 
     public IColumnDefinition<EpicListDto> ProjectId { get; } =
-        new ColumnDefinition<EpicListDto>("ProjectId", x => x.ProjectId, ColumnAlignment.Left, "1", null);
+        new ColumnDefinition<EpicListDto>("ProjectId", typeof(int), x => x.ProjectId, ColumnAlignment.Left, "1", null);
 
     public override IEnumerable<IColumnDefinition<EpicListDto>> GetColumns()
     {
--- src/Jared.Presentation/ColumnDefinitions/TaskListDefinition.cs
+++ src/Jared.Presentation/ColumnDefinitions/TaskListDefinition.cs
@@ -1,31 +1,32 @@
 ﻿using Jared.Application.Dtos.TaskDtos;
 using Jared.Domain.Enums;
 using Jared.Presentation.ColumnDefinitions.Abstraction;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Jared.Presentation.ColumnDefinitions;
 
 public class TaskListDefinition : ListDefinitionBase<TaskListDto>
 {
     public IColumnDefinition<TaskListDto> Id { get; } =
-        new ColumnDefinition<TaskListDto>("Id", x => x.Id, ColumnAlignment.Left, "1", null);
+        new ColumnDefinition<TaskListDto>("Id", typeof(int), x => x.Id, ColumnAlignment.Left, "1", null);
 
     public IColumnDefinition<TaskListDto> Title { get; } =
-        new ColumnDefinition<TaskListDto>("Title", x => x.Title, ColumnAlignment.Left, "5", null);
+        new ColumnDefinition<TaskListDto>("Title", typeof(string), x => x.Title, ColumnAlignment.Left, "5", null);
 
     public IColumnDefinition<TaskListDto> Code { get; } =
-        new ColumnDefinition<TaskListDto>("Code", x => x.Code, ColumnAlignment.Left, "2", null);
+        new ColumnDefinition<TaskListDto>("Code", typeof(string), x => x.Code, ColumnAlignment.Left, "2", null);
 
     public IColumnDefinition<TaskListDto> Priority { get; } =
-        new ColumnDefinition<TaskListDto>("Priority", x => x.Priority, ColumnAlignment.Left, "2", null);
+        new ColumnDefinition<TaskListDto>("Priority", typeof(Priority), x => x.Priority, ColumnAlignment.Left, "2", null);
 
     public IColumnDefinition<TaskListDto> Status { get; } =
-        new ColumnDefinition<TaskListDto>("Status", x => x.Status, ColumnAlignment.Left, "2", null);
+        new ColumnDefinition<TaskListDto>("Status", typeof(Domain.Enums.TaskStatus), x => x.Status, ColumnAlignment.Left, "2", null);
 
     public IColumnDefinition<TaskListDto> CreatedAt { get; } =
-        new ColumnDefinition<TaskListDto>("CreatedAt", x => x.CreatedAt, ColumnAlignment.Left, "2", "dd/MM/yyyy");
+        new ColumnDefinition<TaskListDto>("CreatedAt", typeof(DateTime), x => x.CreatedAt, ColumnAlignment.Left, "2", "dd/MM/yyyy");
 
     public IColumnDefinition<TaskListDto> Deadline { get; } =
-        new ColumnDefinition<TaskListDto>("Deadline", x => x.Deadline, ColumnAlignment.Left, "2", "dd/MM/yyyy");
+        new ColumnDefinition<TaskListDto>("Deadline", typeof(DateTime?), x => x.Deadline, ColumnAlignment.Left, "2", "dd/MM/yyyy");
 
     public override IEnumerable<IColumnDefinition<TaskListDto>> GetColumns()
     {
