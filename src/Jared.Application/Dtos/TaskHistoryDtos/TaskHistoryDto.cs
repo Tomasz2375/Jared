7336707b84a42a3f8acdf66aa4d1e89bcf45b757
Tomasz <trnosal@gmail.com>
2024-05-02 10:50:10 +0200

    Add TaskHistoryDto

---
+++ src/Jared.Application/Dtos/TaskHistoryDtos/TaskHistoryDto.cs
@@ -0,0 +1,11 @@
+﻿using Jared.Application.Dtos.Abstractions;
+
+namespace Jared.Application.Dtos.TaskHistoryDtos;
+
+public class TaskHistoryDto : EntityDto<int>
+{
+    public string Property { get; set; } = default!;
+    public string? OldValue { get; set; }
+    public string? NewValue { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
