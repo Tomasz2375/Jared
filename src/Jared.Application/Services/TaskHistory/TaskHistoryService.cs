2e0605ca87c1b05a02d8484a59c0f60fc30568bc
Tomasz <trnosal@gmail.com>
2024-05-02 10:50:35 +0200

    Add TaskHistoryService

---
+++ src/Jared.Application/Services/TaskHistory/TaskHistoryService.cs
@@ -0,0 +1,35 @@
+﻿using Jared.Application.Dtos.TaskDtos;
+using Jared.Application.Dtos.TaskHistoryDtos;
+
+namespace Jared.Application.Services.TaskHistory;
+
+public class TaskHistoryService : ITaskHistoryService
+{
+    public List<TaskHistoryDto> GetChanged(
+        TaskDetailsDto oldTaskDetails,
+        TaskDetailsDto newTaskDetails)
+    {
+        List<TaskHistoryDto> changed = new();
+
+        foreach (var property in typeof(TaskDetailsDto).GetProperties())
+        {
+            var oldProperty = oldTaskDetails.GetType().GetProperty(property.Name)!.GetValue(oldTaskDetails);
+            var newProperty = newTaskDetails.GetType().GetProperty(property.Name)!.GetValue(newTaskDetails);
+            var oldValue = oldProperty is null ? null : oldProperty.ToString();
+            var newValue = newProperty is null ? null : newProperty.ToString();
+
+
+            if (newValue != oldValue)
+            {
+                changed.Add(new()
+                {
+                    Property = property.Name,
+                    OldValue = oldValue,
+                    NewValue = newValue!,
+                });
+            }
+        }
+
+        return changed;
+    }
+}
