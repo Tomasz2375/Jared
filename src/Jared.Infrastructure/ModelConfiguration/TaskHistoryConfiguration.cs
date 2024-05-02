29e9891770d0bae47ea1f6bbbfbe95fed0351704
Tomasz <trnosal@gmail.com>
2024-05-02 10:49:35 +0200

    Add TaskHistoryConfiguration

---
+++ src/Jared.Infrastructure/ModelConfiguration/TaskHistoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Jared.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Jared.Infrastructure.ModelConfiguration;
+
+public class TaskHistoryConfiguration : IEntityTypeConfiguration<TaskHistory>
+{
+    public void Configure(EntityTypeBuilder<TaskHistory> builder)
+    {
+        builder.HasOne(th => th.Task)
+            .WithMany(t => t.TaskHistories)
+            .HasForeignKey(th => th.TaskId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(x => x.CreatedAt)
+            .HasDefaultValueSql("getdate()");
+    }
+}
