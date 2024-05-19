bde5cf4c639b635631282ca21c4d7e96a2c49e27
Tomasz <trnosal@gmail.com>
2024-05-19 19:03:02 +0200

    Fix TaskRootDto

--- src/Jared.Application/Dtos/TaskDtos/TaskRootDto.cs
+++ src/Jared.Application/Dtos/TaskDtos/TaskRootDto.cs
@@ -2,19 +2,18 @@
 using Jared.Domain.Enums;
 using TaskStatus = Jared.Domain.Enums.TaskStatus;
 
-namespace Jared.Application.Dtos.TaskDtos
+namespace Jared.Application.Dtos.TaskDtos;
+
+public class TaskRootDto : EntityDto<int>
 {
-    public class TaskRootDto : EntityDto<int>
-    {
-        public string Title { get; set; } = default!;
-        public int ProjectId { get; set; }
-        public string? Code { get; set; }
-        public int? EpicId { get; set; }
+    public string Title { get; set; } = default!;
+    public int ProjectId { get; set; }
+    public string? Code { get; set; }
+    public int? EpicId { get; set; }
 
-        public TaskStatus Status { get; set; }
-        public Priority Priority { get; set; }
+    public TaskStatus Status { get; set; }
+    public Priority Priority { get; set; }
 
-        public DateTime CreatedAt { get; set; }
-        public DateTime? Deadline { get; set; }
-    }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? Deadline { get; set; }
 }
