cce28b7a1ee7e551b198d982af3754d98b55208d
Tomasz <trnosal@gmail.com>
2024-06-01 18:24:16 +0200

    Add entities

---
+++ src/Jared.Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Jared.Domain.Abstractions;
+
+namespace Jared.Domain.Models;
+
+public class User : Entity
+{
+    public int RoleId { get; set; }
+
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public DateTime? DateOfBirth { get; set; }
+    public string Email { get; set; } = default!;
+    public string PasswordHash { get; set; } = default!;
+
+    public Role? Role { get; set; }
+}
