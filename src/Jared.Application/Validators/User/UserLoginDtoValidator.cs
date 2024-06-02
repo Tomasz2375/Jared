186f7784f7a34ebeefa35fb446d2b60dac3f8225
Tomasz <trnosal@gmail.com>
2024-06-02 14:47:36 +0200

    Add UserRegisterDtoValidator

---
+++ src/Jared.Application/Validators/User/UserLoginDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Jared.Application.Dtos.UserDtos;
+
+namespace Jared.Application.Validators.User;
+
+public class UserLoginDtoValidator : AbstractValidator<UserLoginDto>
+{
+    public UserLoginDtoValidator()
+    {
+        RuleFor(x => x.Email).NotEmpty().EmailAddress();
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(6).MaximumLength(50);
+    }
+}
