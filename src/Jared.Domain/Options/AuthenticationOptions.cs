eec7d37147613b11d6eec429b4ab24e9c2e326f4
Tomasz <trnosal@gmail.com>
2024-06-16 12:20:43 +0200

    Create authentication options

---
+++ src/Jared.Domain/Options/AuthenticationOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Jared.Domain.Options;
+
+public class AuthenticationOptions
+{
+    public static string Section { get; } = "Authentication";
+
+    public string JwtKey { get; set; } = default!;
+    public string JwtIssurer { get; set; } = default!;
+    public int JwtExpireDays { get; set; }
+}
