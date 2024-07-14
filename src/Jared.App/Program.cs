ffd4a93e7f97699b57ff923ed0930483342e88b4
Tomasz <trnosal@gmail.com>
2024-07-14 10:44:45 +0200

    Create project Jared.App

---
+++ src/Jared.App/Program.cs
@@ -0,0 +1,10 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
