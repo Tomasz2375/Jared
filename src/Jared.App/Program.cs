0d6138ef6a3505c74ec233e72baaf74f4b82c054
Tomasz <trnosal@gmail.com>
2024-07-20 15:57:02 +0200

    Rename app project

--- src/Jared.App/Program.cs
+++ src/Jared.App/Program.cs
@@ -1,17 +1,17 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddMvcCore();
+
 builder.Services.AddHttpClient("Jared.Api", client =>
 {
     client.BaseAddress = new Uri("https://localhost:7050/api/");
-});
 
+});
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
@@ -27,24 +27,5 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-app.UseAuthentication();
-app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapRazorPages();
-    endpoints.MapBlazorHub();
-    endpoints.MapFallbackToPage("/_Host");
-});
 app.Run();
-
-//var builder = WebAssemblyHostBuilder.CreateDefault(args);
-//builder.RootComponents.Add<App>("#app");
-//builder.RootComponents.Add<HeadOutlet>("head::after");
-
-//builder.Services.AddBlazoredLocalStorage();
-//builder.Services.AddRazorPages();
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7050") });
-//builder.Services.AddOptions();
-//builder.Services.AddAuthorizationCore();
-//await builder.Build().RunAsync();
