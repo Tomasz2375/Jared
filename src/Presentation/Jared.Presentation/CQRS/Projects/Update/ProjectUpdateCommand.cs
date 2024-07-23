cc39c6ad0a93b1e141512ed60f961d1867a1a76f
Tomasz <trnosal@gmail.com>
2024-07-23 18:49:15 +0200

    Add crud operation for project

---
+++ src/Presentation/Jared.Presentation/CQRS/Projects/Update/ProjectUpdateCommand.cs
@@ -0,0 +1,7 @@
+﻿using Jared.Application.Dtos.ProjectDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Presentation.CQRS.Projects.Update;
+
+public sealed record ProjectUpdateCommand(ProjectDetailsDto dto) : IRequest<Result>;
