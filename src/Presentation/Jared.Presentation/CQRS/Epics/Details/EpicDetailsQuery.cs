c90fec845e09b0f442f581bd6423dd14371464f8
Tomasz <trnosal@gmail.com>
2024-07-23 18:48:46 +0200

    Add crud operation for epic

---
+++ src/Presentation/Jared.Presentation/CQRS/Epics/Details/EpicDetailsQuery.cs
@@ -0,0 +1,7 @@
+﻿using Jared.Application.Dtos.EpicDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Presentation.CQRS.Epics.Details;
+
+public sealed record EpicDetailsQuery(int id) : IRequest<Result<EpicDetailsDto>>;
