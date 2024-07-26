4024425787938d4d8faac02a31344a414304c0e3
Tomasz <trnosal@gmail.com>
2024-07-26 18:14:06 +0200

    Register services in Jared.App and modify host and main layout

--- src/Jared.App/Program.cs
+++ src/Jared.App/Program.cs
@@ -1,15 +1,31 @@
+using Jared.App;
 using Jared.Presentation;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddPresentation();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddPresentation();
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    x.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    x.DefaultSignOutScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+});
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+
 builder.Services.AddScoped(client => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7050/api/")
 });
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
 var app = builder.Build();
 
@@ -26,7 +42,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
