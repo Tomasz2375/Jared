285fbdd286f3f0767e01f4c80aebdcd84c5d41de
Tomasz <trnosal@gmail.com>
2024-07-26 18:15:58 +0200

    Add UserRegisterCommand app

---
+++ src/Presentation/Jared.Presentation/CQRS/User/Register/UserRegisterCommand.cs
@@ -0,0 +1,7 @@
+﻿using Jared.Application.Dtos.UserDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Presentation.CQRS.User.Register;
+
+public sealed record UserRegisterCommand(UserRegisterDto dto) : IRequest<Result>;
---
+++ src/Presentation/Jared.Presentation/CQRS/User/Register/UserRegisterCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Jared.Domain.Abstractions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Json;
+
+namespace Jared.Presentation.CQRS.User.Register;
+
+public class UserRegisterCommandHandler : IRequestHandler<UserRegisterCommand, Result>
+{
+    private readonly HttpClient httpClient;
+
+    public UserRegisterCommandHandler(HttpClient httpClient)
+    {
+        this.httpClient = httpClient;
+    }
+
+    public async Task<Result> Handle(UserRegisterCommand request, CancellationToken cancellationToken)
+    {
+        var baseUrl = "User/Register";
+
+        var result = await httpClient.PostAsJsonAsync(baseUrl, request.dto, cancellationToken);
+
+        // var test = await result.Content.ReadFromJsonAsync<ActionResult<Result>>();
+
+        return Result.Ok();
+    }
+}
