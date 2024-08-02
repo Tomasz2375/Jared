08e7a6fd0c1cd869bb291d9dab982ce004d8b6de
Tomasz <trnosal@gmail.com>
2024-08-02 18:58:04 +0200

    Create user service

---
+++ src/Application/Jared.Application/Services/User/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Jared.Application.Services.User;
+
+public class UserService : IUserService
+{
+    private readonly IHttpContextAccessor httpContextAccessor;
+
+    public UserService(IHttpContextAccessor httpContextAccessor)
+    {
+        this.httpContextAccessor = httpContextAccessor;
+    }
+
+    public Domain.Models.User GetUser()
+    {
+        var claimId = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var claimName = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Name)?.Value;
+        var claimRole = httpContextAccessor.HttpContext?.User.FindFirst(ClaimTypes.Role)?.Value;
+        var claimDateOfBirth = httpContextAccessor.HttpContext?.User.FindFirst("DateOfBirth")?.Value;
+
+        Domain.Models.User user = new();
+
+        if (int.TryParse(claimId, out int id))
+        {
+            user.Id = id;
+        }
+        if (DateTime.TryParse(claimDateOfBirth, out DateTime dateOfBirth))
+        {
+            user.DateOfBirth = dateOfBirth;
+        }
+        if (claimName != null)
+        {
+            user.FirstName = claimName.Split(" ")[0];
+            user.LastName = claimName.Split(" ")[1];
+        }
+
+        return user;
+    }
+}
