dfc1722105a620f534cba3fa275cfba81602342b
Tomasz <trnosal@gmail.com>
2024-08-03 12:53:51 +0200

    Add UserPasswordCommand api

---
+++ src/Application/Jared.Application/Commands/UserCommands/UserPasswordCommand.cs
@@ -0,0 +1,7 @@
+﻿using Jared.Application.Dtos.UserDtos;
+using Jared.Domain.Abstractions;
+using MediatR;
+
+namespace Jared.Application.Commands.UserCommands;
+
+public sealed record UserPasswordCommand(UserPasswordDto dto) : IRequest<Result<bool>>;
