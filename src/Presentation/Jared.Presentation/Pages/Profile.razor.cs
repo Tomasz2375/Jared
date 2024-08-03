99c5cba02a7c3625f0bf701fbea14f2942c29f88
Tomasz <trnosal@gmail.com>
2024-08-03 12:56:25 +0200

    Add profile page

---
+++ src/Presentation/Jared.Presentation/Pages/Profile.razor.cs
@@ -0,0 +1,27 @@
+﻿using Jared.Application.Dtos.UserDtos;
+using Jared.Presentation.CQRS.User.Password;
+
+namespace Jared.Presentation.Pages;
+
+public partial class Profile
+{
+    private string message = string.Empty;
+    private bool isPasswordChanged;
+    private UserPasswordDto dto = new();
+
+    private async Task changePassword()
+    {
+        var result = await Mediator.Send(new UserPasswordCommand(dto));
+
+        if (!result.Success)
+        {
+            message = result.Error;
+            isPasswordChanged = false;
+        }
+        else
+        {
+            message = "Password has been changed";
+            isPasswordChanged = true;
+        }
+    }
+}
