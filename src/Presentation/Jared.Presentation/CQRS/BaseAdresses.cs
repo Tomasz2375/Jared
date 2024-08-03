115b1d4dd07a4bcce75afa4f5ca530e27abd480b
Tomasz <trnosal@gmail.com>
2024-08-03 12:56:11 +0200

    Update base addresses

--- src/Presentation/Jared.Presentation/CQRS/BaseAdresses.cs
+++ src/Presentation/Jared.Presentation/CQRS/BaseAdresses.cs
@@ -3,31 +3,32 @@
 public static class BaseAdresses
 {
     #region Tasks
-    public const string TASK_CREATE = "Task/Create";
-    public const string TASK_UPDATE = "Task/Update";
-    public const string TASK_DETAILS = "Task";
-    public const string TASK_LIST = "Task/List";
-    public const string TASK_PAGE = "Task/Page";
+    public const string TASK_CREATE = "task/create";
+    public const string TASK_UPDATE = "task/update";
+    public const string TASK_DETAILS = "task";
+    public const string TASK_LIST = "task/list";
+    public const string TASK_PAGE = "task/page";
     #endregion
 
     #region Epics
-    public const string EPIC_CREATE = "Epic/Create";
-    public const string EPIC_UPDATE = "Epic/Update";
-    public const string EPIC_DETAILS = "Epic";
-    public const string EPIC_LIST = "Epic/List";
-    public const string EPIC_PAGE = "Epic/Page";
+    public const string EPIC_CREATE = "epic/create";
+    public const string EPIC_UPDATE = "epic/update";
+    public const string EPIC_DETAILS = "epic";
+    public const string EPIC_LIST = "epic/list";
+    public const string EPIC_PAGE = "epic/page";
     #endregion
 
     #region Projects
-    public const string PROJECT_CREATE = "Project/Create";
-    public const string PROJECT_UPDATE = "Project/Update";
-    public const string PROJECT_DETAILS = "Project";
-    public const string PROJECT_LIST = "Project/List";
-    public const string PROJECT_PAGE = "Project/Page";
+    public const string PROJECT_CREATE = "project/create";
+    public const string PROJECT_UPDATE = "project/update";
+    public const string PROJECT_DETAILS = "project";
+    public const string PROJECT_LIST = "project/list";
+    public const string PROJECT_PAGE = "project/page";
     #endregion
 
     #region Authorization
-    public const string USER_LOGIN = "User/Login";
-    public const string USER_REGISTER = "User/Register";
+    public const string USER_LOGIN = "user/login";
+    public const string USER_REGISTER = "user/register";
+    public const string USER_PASSWORD = "user/password";
     #endregion
 }
