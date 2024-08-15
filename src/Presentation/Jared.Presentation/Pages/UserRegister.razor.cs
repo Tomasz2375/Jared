2ccdc5101fc348863fa7031f8813a6a36a257bdd
Tomasz <trnosal@gmail.com>
2024-08-15 12:05:27 +0200

    Update user register page

--- src/Presentation/Jared.Presentation/Pages/UserRegister.razor.cs
+++ src/Presentation/Jared.Presentation/Pages/UserRegister.razor.cs
@@ -13,7 +13,9 @@ public partial class UserRegister
         var result = await Mediator.Send(new UserRegisterCommand(Dto));
         if (!result.Success)
         {
-            Console.WriteLine("Register user failed");
+            Console.WriteLine(result.Error);
+
+            return;
         }
 
         NavigationManager.NavigateTo("login");
