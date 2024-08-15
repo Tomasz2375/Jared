a0af29dc0400ac5a6bb5b851c7f0485750268f36
Tomasz <trnosal@gmail.com>
2024-08-15 12:01:57 +0200

    Fix user configuration

--- src/Infrastructure/Jared.Infrastructure/ModelConfiguration/UserConfiguration.cs
+++ src/Infrastructure/Jared.Infrastructure/ModelConfiguration/UserConfiguration.cs
@@ -15,5 +15,6 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
         builder.Property(x => x.FirstName).HasMaxLength(50).IsRequired();
         builder.Property(x => x.LastName).HasMaxLength(50).IsRequired();
         builder.Property(x => x.Email).HasMaxLength(50).IsRequired();
+        builder.HasIndex(x => x.Email).IsUnique();
     }
 }
