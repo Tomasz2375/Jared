50cdcd4681c137cde20c21be6b8c461c1d844f7c
Tomasz <trnosal@gmail.com>
2024-08-26 17:23:13 +0200

    Add integration fakers

---
+++ test-integration/Jared.Api.Integration.Tests/Data/EpicIntegrationFaker.cs
@@ -0,0 +1,44 @@
+﻿using Jared.Domain.Models;
+
+namespace Jared.Api.Integration.Tests.Data;
+
+public class EpicIntegrationFaker : BaseIntegrationFaker<Epic>
+{
+    protected override int PRIORITY => 4;
+
+    public static Epic FirstEpic => new()
+    {
+        Id = BASE_ID + 1,
+        ProjectId = ProjectIntegrationFaker.FirstProject.Id,
+        ParentId = null,
+        Title = "FIRST_EPIC_TITLE",
+        Description = "FIRST_EPIC_DESCRIPTION",
+    };
+
+    public static Epic SecondEpic => new()
+    {
+        Id = BASE_ID + 2,
+        ProjectId = ProjectIntegrationFaker.FirstProject.Id,
+        ParentId = FirstEpic.Id,
+        Title = "SECOND_EPIC_TITLE",
+        Description = "SECOND_EPIC_DESCRIPTION",
+    };
+
+    public static Epic ThirdEpic => new()
+    {
+        Id = BASE_ID + 3,
+        ProjectId = ProjectIntegrationFaker.SecondProject.Id,
+        ParentId = null,
+        Title = "THIRD_EPIC_TITLE",
+        Description = "THIRS_EPIC_DESCRIPTION",
+    };
+
+    public static Epic FourthEpic => new()
+    {
+        Id = BASE_ID + 4,
+        ProjectId = ProjectIntegrationFaker.ThirdProject.Id,
+        ParentId = null,
+        Title = "FOURTH_EPIC_TITLE",
+        Description = "FOURTH_EPIC_DESCRIPTION",
+    };
+}
