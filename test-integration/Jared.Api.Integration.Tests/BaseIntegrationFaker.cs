7b684b1ce15052ad88b0752d1a6ce62fcd174e08
Tomasz <trnosal@gmail.com>
2024-08-26 17:22:24 +0200

    Create BaseIntegrationFaker

---
+++ test-integration/Jared.Api.Integration.Tests/BaseIntegrationFaker.cs
@@ -0,0 +1,11 @@
+﻿using Jared.Shared.Interfaces;
+
+namespace Jared.Api.Integration.Tests;
+
+public abstract class BaseIntegrationFaker<TEntity>
+    where TEntity : class, IEntity
+{
+    protected static int BASE_ID { get; set; } = 1000;
+
+    protected abstract int PRIORITY { get; }
+}
