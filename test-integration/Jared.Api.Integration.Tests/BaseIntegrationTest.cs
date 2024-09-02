24c2324d7a5cfa661fa20b04bc4328f64898481e
Tomasz <trnosal@gmail.com>
2024-09-02 20:56:07 +0200

    Modify BaseIntegrationTest

--- test-integration/Jared.Api.Integration.Tests/BaseIntegrationTest.cs
+++ test-integration/Jared.Api.Integration.Tests/BaseIntegrationTest.cs
@@ -1,27 +1,22 @@
-﻿using Jared.Shared.Interfaces;
-using Microsoft.Extensions.DependencyInjection;
-
-namespace Jared.Api.Integration.Tests;
+﻿namespace Jared.Api.Integration.Tests;
 
 [Collection("TestCollection")]
 public abstract class BaseIntegrationTest : IDisposable
 {
-    private readonly IServiceScope scope;
-
-    protected readonly IDataContext DataContext;
+    private readonly JaredWebApplicationFactory factory;
+    protected abstract string URL { get; }
     protected HttpClient Client { get; }
 
     protected BaseIntegrationTest(JaredWebApplicationFactory factory)
     {
+        this.factory = factory;
         Client = factory.CreateClient();
         Client.BaseAddress = new Uri("https://localhost:7050/api/");
-
-        scope = factory.Services.CreateScope();
-        DataContext = scope.ServiceProvider.GetRequiredService<IDataContext>();
     }
 
     public void Dispose()
     {
+        factory.RestoreSnapshot();
         GC.SuppressFinalize(this);
     }
 }
