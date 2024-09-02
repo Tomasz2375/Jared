7375d81d661dfd73a1b903060a547f4e5ccc7eba
Tomasz <trnosal@gmail.com>
2024-09-02 20:59:18 +0200

    Add ProjectListTest

---
+++ test-integration/Jared.Api.Integration.Tests/Tests/Projects/List/ProjectListTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using Jared.Api.Integration.Tests.Data;
+using Jared.Shared.Abstractions;
+using Jared.Shared.Dtos.ProjectDtos;
+using Mapster;
+using System.Net.Http.Json;
+
+namespace Jared.Api.Integration.Tests.Tests.Projects.List;
+
+public class ProjectListTest : BaseIntegrationTest
+{
+    protected override string URL => "Project/List";
+
+    public ProjectListTest(JaredWebApplicationFactory factory) : base(factory)
+    {
+    }
+
+    [Fact]
+    public async Task DetailsProject_WhenProjectExists_ShoutdBeSuccess()
+    {
+        // Arrange
+        Result<List<ProjectListDto>> expectedResponse = new(true, string.Empty)
+        {
+            Data = new()
+            {
+                ProjectIntegrationFaker.FirstProject.Adapt<ProjectListDto>(),
+                ProjectIntegrationFaker.SecondProject.Adapt<ProjectListDto>(),
+                ProjectIntegrationFaker.ThirdProject.Adapt<ProjectListDto>(),
+            },
+        };
+
+        // Act
+        var response = await Client.GetFromJsonAsync<Result<List<ProjectListDto>>>(URL, default);
+
+        // Assert
+        expectedResponse.Should().BeEquivalentTo(response);
+    }
+}
