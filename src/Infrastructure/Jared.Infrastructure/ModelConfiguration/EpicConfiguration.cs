52592afd8e4dc5fc23489bb707e5b76c84fd2d72
Tomasz <trnosal@gmail.com>
2024-09-08 11:38:11 +0200

    Update epic configuration

--- src/Infrastructure/Jared.Infrastructure/ModelConfiguration/EpicConfiguration.cs
+++ src/Infrastructure/Jared.Infrastructure/ModelConfiguration/EpicConfiguration.cs
@@ -12,5 +12,10 @@ public class EpicConfiguration : IEntityTypeConfiguration<Epic>
             .WithMany(p => p.Epics)
             .HasForeignKey(e => e.ProjectId)
             .OnDelete(DeleteBehavior.NoAction);
+
+        builder.Property(x => x.CreatedAt)
+            .HasDefaultValueSql("getdate()");
+        builder.Property(x => x.ProjectId).IsRequired();
+        builder.Property(x => x.Title).IsRequired().HasMaxLength(100);
     }
 }
