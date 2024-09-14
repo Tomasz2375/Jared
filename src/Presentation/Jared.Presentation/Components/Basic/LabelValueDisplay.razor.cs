ca2a85ac99e07e08bcfa1ff54c0dc0aae5e65d71
Tomasz <trnosal@gmail.com>
2024-09-14 22:16:35 +0200

    Create LabelValueDisplay component

---
+++ src/Presentation/Jared.Presentation/Components/Basic/LabelValueDisplay.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Jared.Presentation.Components.Basic;
+
+public partial class LabelValueDisplay
+{
+    [Parameter]
+    public string? Id { get; set; }
+    [Parameter]
+    public string? Label { get; set; }
+    [Parameter]
+    [EditorRequired]
+    public string? Value { get; set; }
+}
