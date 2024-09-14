085d1f07dc5e38a35ba570bf33e922486a17c0dd
Tomasz <trnosal@gmail.com>
2024-09-14 23:24:12 +0200

    Update dtos

--- src/Shared/Jared.Shared/Dtos/EpicDtos/EpicDetailsDto.cs
+++ src/Shared/Jared.Shared/Dtos/EpicDtos/EpicDetailsDto.cs
@@ -5,5 +5,10 @@ namespace Jared.Shared.Dtos.EpicDtos;
 public class EpicDetailsDto : EpicRootDto
 {
     public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public DateTime? Deadline { get; set; }
+
     public IEnumerable<TaskListDto> Tasks { get; set; } = Array.Empty<TaskListDto>();
 }
