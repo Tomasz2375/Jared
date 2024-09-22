e18d557266cb1d889e7fb3b76c0df2d7d4b7c8f6
Tomasz <trnosal@gmail.com>
2024-09-22 18:12:23 +0200

    Add filters for entities

---
+++ src/Application/Jared.Application/Services/Filters/EpicFilter.cs
@@ -0,0 +1,45 @@
+﻿using Jared.Domain.Models;
+using Jared.Shared.Dtos.EpicDtos;
+
+namespace Jared.Application.Services.Filters;
+
+public class EpicFilter : IFilterStrategy<Epic>
+{
+    public IQueryable<Epic> ApplyFilter(IQueryable<Epic> query, string key, string value)
+    {
+        if (filters.TryGetValue(key, out var filter))
+        {
+            query = filter(query, value);
+        }
+
+        return query;
+    }
+
+    private Dictionary<string, Func<IQueryable<Epic>, string, IQueryable<Epic>>> filters = new()
+    {
+        {
+            nameof(EpicListDto.Id),
+            (epics, value) => epics.Where(x => x.Id.ToString().Contains(value))
+        },
+        {
+            nameof(EpicListDto.Title),
+            (epics, value) => epics.Where(x => x.Title.Contains(value))
+        },
+        {
+            nameof(EpicListDto.ParentId),
+            (epics, value) => epics.Where(x => x.ParentId.ToString()!.Contains(value))
+        },
+        {
+            nameof(EpicListDto.ProjectId),
+            (epics, value) => epics.Where(x => x.ProjectId.ToString().Contains(value))
+        },
+        {
+            nameof(EpicListDto.Status),
+            (epics, value) =>
+            {
+                if (value == "0") return epics;
+                return epics.Where(x => ((int)x.Status & int.Parse(value)) != 0);
+            }
+        },
+    };
+}
