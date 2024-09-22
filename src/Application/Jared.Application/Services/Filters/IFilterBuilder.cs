c8945338d6b7762ec5c60e5d430762ede8dadb8c
Tomasz <trnosal@gmail.com>
2024-09-22 18:12:02 +0200

    Create filter builder

---
+++ src/Application/Jared.Application/Services/Filters/IFilterBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Jared.Shared.Interfaces;
+
+namespace Jared.Application.Services.Filters;
+
+public interface IFilterBuilder<T> where T : class, IEntity
+{
+    IFilter<T> Build(IFilterStrategy<T> strategy);
+}
