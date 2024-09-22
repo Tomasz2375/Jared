3e116cc5369999bb96ae9e1dea72f738d25e1af7
Tomasz <trnosal@gmail.com>
2024-09-22 18:11:25 +0200

    Create filter strategy

---
+++ src/Application/Jared.Application/Services/Filters/IFilter.cs
@@ -0,0 +1,8 @@
+﻿using Jared.Shared.Interfaces;
+
+namespace Jared.Application.Services.Filters;
+
+public interface IFilter<T> where T : class, IEntity
+{
+    IQueryable<T> ApplyFilters(IQueryable<T> query, string key, string value);
+}
