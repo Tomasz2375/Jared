1e222761f84bb246bb514d6dcc5ce01f7dc826b4
Tomasz <trnosal@gmail.com>
2024-10-04 15:28:59 +0200

    Create ToggleButton component

---
+++ src/Presentation/Jared.Presentation/Components/Basic/ToggleButton.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Jared.Presentation.Components.Basic;
+
+public partial class ToggleButton
+{
+    [Parameter]
+    public bool IsToggled { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> IsToggledChanged { get; set; }
+
+    private void Toggle()
+    {
+        IsToggled = !IsToggled;
+        IsToggledChanged.InvokeAsync(IsToggled);
+    }
+
+    private string buttonClass => IsToggled ? "toggle-on" : "toggle-off";
+    private string buttonText => IsToggled ? "Dark" : "Light";
+}
