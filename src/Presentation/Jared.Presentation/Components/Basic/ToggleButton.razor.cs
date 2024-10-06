6b5a4bcffa73a62aacff65fc194b43ae401658f4
Tomasz <trnosal@gmail.com>
2024-10-06 12:51:07 +0200

    Add color selected to toggle button

--- src/Presentation/Jared.Presentation/Components/Basic/ToggleButton.razor.cs
+++ src/Presentation/Jared.Presentation/Components/Basic/ToggleButton.razor.cs
@@ -8,12 +8,23 @@ public partial class ToggleButton
     public bool IsToggled { get; set; }
 
     [Parameter]
+    public string Color { get; set; } = default!;
+
+    [Parameter]
     public EventCallback<bool> IsToggledChanged { get; set; }
+    [Parameter]
+    public EventCallback<string> OnColorChange { get; set; }
 
     private void Toggle()
     {
         IsToggled = !IsToggled;
         IsToggledChanged.InvokeAsync(IsToggled);
+    }
+
+    private async Task changeColor(ChangeEventArgs e)
+    {
+        var color = e.Value!.ToString();
+        await OnColorChange.InvokeAsync(color);
     }
 
     private string buttonClass => IsToggled ? "toggle-on" : "toggle-off";
