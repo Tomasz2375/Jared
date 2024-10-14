3db250d3c44cf10fcba47cf2f3500b8f46ecc4b5
Tomasz <trnosal@gmail.com>
2024-10-14 18:53:26 +0200

    Add RequestLogContextMiddleware

---
+++ src/Shared/Jared.Shared/Middleware/RequestLogContextMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+
+namespace Jared.Shared.Middleware;
+
+public class RequestLogContextMiddleware
+{
+    private readonly RequestDelegate requestDelegate;
+
+    public RequestLogContextMiddleware(RequestDelegate requestDelegate)
+    {
+        this.requestDelegate = requestDelegate;
+    }
+
+    public Task Invoke(HttpContext httpContext)
+    {
+        using (LogContext.PushProperty("CorrelationId", httpContext.TraceIdentifier))
+        {
+            return requestDelegate(httpContext);
+        }
+    }
+}
