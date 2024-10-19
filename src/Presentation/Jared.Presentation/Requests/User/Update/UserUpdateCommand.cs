fa99843803fa1a8080c5aa919c3bbbebb76f2355
Tomasz <trnosal@gmail.com>
2024-10-19 13:29:41 +0200

    Add user update command in app

---
+++ src/Presentation/Jared.Presentation/Requests/User/Update/UserUpdateCommand.cs
@@ -0,0 +1,7 @@
+﻿using Jared.Shared.Abstractions;
+using Jared.Shared.Dtos.UserDtos;
+using MediatR;
+
+namespace Jared.Presentation.Requests.User.Update;
+
+public sealed record UserUpdateCommand(UserUpdateDto dto) : IRequest<Result<bool>>;
