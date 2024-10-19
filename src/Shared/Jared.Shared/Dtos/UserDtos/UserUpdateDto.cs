fe7fcaadf6f05b54b43ffdc53f2c1455598ca3fa
Tomasz <trnosal@gmail.com>
2024-10-19 13:29:10 +0200

    Create user update dto

---
+++ src/Shared/Jared.Shared/Dtos/UserDtos/UserUpdateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Jared.Shared.Dtos.UserDtos;
+
+public class UserUpdateDto
+{
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public DateTime? DateOfBirth { get; set; }
+}
