26114f4059ff12c99dd99b1123053ed4d2634bc2
Tomasz <trnosal@gmail.com>
2024-11-01 18:06:43 +0100

    Update user dtos

---
+++ src/Shared/Jared.Shared/Dtos/UserDtos/UserRoleUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using Jared.Shared.Dtos.Abstractions;
+
+namespace Jared.Shared.Dtos.UserDtos
+{
+    public class UserRoleUpdateDto : EntityDto<int>
+    {
+        public int RoleId { get; set; }
+    }
+}
