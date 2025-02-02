e9ea86024b38a2fae19ec765f79538fb36be392f
Tomasz <trnosal@gmail.com>
2025-02-02 11:07:08 +0100

    Small fix

--- src/Domain/Jared.Domain/Models/WorkLog.cs
+++ src/Domain/Jared.Domain/Models/WorkLog.cs
@@ -9,7 +9,7 @@ public class WorkLog : Entity
     public int TaskId { get; set; }
     public int UserId { get; set; }
     public TimeSpan Time { get; set; }
-    public DateTime WorkDate { get; set; }
+    public DateTime? WorkDate { get; set; }
     public DateTime LoggedDate { get; set; }
 
     public Task? Task { get; set; }
