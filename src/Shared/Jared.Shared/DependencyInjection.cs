0510803a1f9856ea14bfb8d1482d13dc2d99c8aa
Tomasz <trnosal@gmail.com>
2025-02-03 20:03:12 +0100

    Create WorkLogAddDtoValidator

--- src/Shared/Jared.Shared/DependencyInjection.cs
+++ src/Shared/Jared.Shared/DependencyInjection.cs
@@ -2,9 +2,11 @@
 using Jared.Shared.Dtos.EpicDtos;
 using Jared.Shared.Dtos.TaskDtos;
 using Jared.Shared.Dtos.UserDtos;
+using Jared.Shared.Dtos.WorkLogDtos;
 using Jared.Shared.Validators.Epic;
 using Jared.Shared.Validators.Task;
 using Jared.Shared.Validators.User;
+using Jared.Shared.Validators.WorkLog;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jared.Shared;
@@ -18,6 +20,7 @@ public static class DependencyInjection
         services.AddScoped<IValidator<UserRegisterDto>, UserRegisterDtoValidator>();
         services.AddScoped<IValidator<UserLoginDto>, UserLoginDtoValidator>();
         services.AddScoped<IValidator<UserPasswordDto>, UserPasswordDtoValidator>();
+        services.AddScoped<IValidator<WorkLogAddDto>, WorkLogAddDtoValidator>();
 
         return services;
     }
