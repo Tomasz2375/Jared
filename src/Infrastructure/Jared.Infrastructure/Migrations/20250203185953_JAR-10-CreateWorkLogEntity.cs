50c6ee5c8169ad4c3825b5640e58adeeac472367
Tomasz <trnosal@gmail.com>
2025-02-03 20:03:25 +0100

    Create migration JAR-10

---
+++ src/Infrastructure/Jared.Infrastructure/Migrations/20250203185953_JAR-10-CreateWorkLogEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jared.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class JAR10CreateWorkLogEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkLog",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Time = table.Column<TimeSpan>(type: "time", nullable: false),
+                    WorkDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LoggedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkLog", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkLog_Task_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Task",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_WorkLog_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkLog_TaskId",
+                table: "WorkLog",
+                column: "TaskId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkLog_UserId",
+                table: "WorkLog",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkLog");
+        }
+    }
+}
