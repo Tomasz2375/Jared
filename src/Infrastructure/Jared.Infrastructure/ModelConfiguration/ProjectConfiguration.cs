9b5e739a87673a84a1e768abce0cb55af843bfea
Tomasz <trnosal@gmail.com>
2025-02-15 11:20:33 +0100

    Update Project entity and add configuration

---
+++ src/Infrastructure/Jared.Infrastructure/ModelConfiguration/ProjectConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Jared.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Jared.Infrastructure.ModelConfiguration;
+
+public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.Property(x => x.Color)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
