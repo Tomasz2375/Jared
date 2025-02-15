0c9fb7000f00abb018ceecf8b34ed9c2d21ff3c3
Tomasz <trnosal@gmail.com>
2025-02-15 11:26:06 +0100

    Add WorkLogStatisticsQuery api

---
+++ src/Application/Jared.Application/Requests/WorkLogs/Statistics/WorkLogStatisticsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Jared.Shared.Abstractions;
+using Jared.Shared.Dtos.WorkLogDtos;
+using MediatR;
+
+namespace Jared.Application.Requests.WorkLogs.Statistics;
+
+public sealed record WorkLogStatisticsQuery(int userId, int month, int year)
+    : IRequest<Result<List<WorkLogStatisticsDto>>>;
---
+++ src/Application/Jared.Application/Requests/WorkLogs/Statistics/WorkLogStatisticsQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using Jared.Domain.Model;
+using Jared.Shared.Abstractions;
+using Jared.Shared.Dtos.WorkLogDtos;
+using Jared.Shared.Interfaces;
+using MapsterMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jared.Application.Requests.WorkLogs.Statistics;
+
+public class WorkLogStatisticsQueryHandler(IDataContext dataContext, IMapper mapper)
+    : IRequestHandler<WorkLogStatisticsQuery, Result<List<WorkLogStatisticsDto>>>
+{
+    private readonly IDataContext dataContext = dataContext;
+    private readonly IMapper mapper = mapper;
+
+    public async Task<Result<List<WorkLogStatisticsDto>>> Handle(
+        WorkLogStatisticsQuery request,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var workLogQuery = dataContext
+                .Set<WorkLog>()
+                .Include(x => x.Task)
+                .ThenInclude(x => x!.Project)
+                .Where(x =>
+                    x.UserId == request.userId &&
+                    x.WorkDate.Month == request.month &&
+                    x.WorkDate.Year == request.year)
+                .AsNoTracking();
+
+            var workLog = await workLogQuery.ToListAsync(cancellationToken);
+
+            var result = mapper.Map<List<WorkLogStatisticsDto>>(workLog);
+
+            return Result.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            return Result.Fail<List<WorkLogStatisticsDto>>(ex.Message);
+        }
+    }
+}
