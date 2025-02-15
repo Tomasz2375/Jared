08f69d95bd52ccdb8e4614acab58f7592aa953a3
Tomasz <trnosal@gmail.com>
2025-02-15 11:26:49 +0100

    Create WorkLogStatisticsQuery app

---
+++ src/Presentation/Jared.Presentation/Requests/WorkLogs/Statistics/WorkLogStatisticsQuery.cs
@@ -0,0 +1,8 @@
+﻿using Jared.Shared.Abstractions;
+using Jared.Shared.Dtos.WorkLogDtos;
+using MediatR;
+
+namespace Jared.Presentation.Requests.WorkLogs.Statistics;
+
+public sealed record WorkLogStatisticsQuery(int userId, int month, int year)
+    : IRequest<Result<List<WorkLogStatisticsDto>>>;
