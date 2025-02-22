2db965f4587a8df34c12b8d2a1a716682b5a25ae
Tomasz <trnosal@gmail.com>
2025-02-22 15:23:45 +0100

    Modify WorkLogStatisticsDto

--- src/Shared/Jared.Shared/Dtos/WorkLogDtos/WorkLogStatisticsDto.cs
+++ src/Shared/Jared.Shared/Dtos/WorkLogDtos/WorkLogStatisticsDto.cs
@@ -4,6 +4,7 @@ public class WorkLogStatisticsDto
 {
     public int TaskId { get; set; }
     public string TaskCode { get; set; } = default!;
+    public string TaskTitle { get; set; } = default!;
     public int ProjectId { get; set; }
     public string ProjectTitle { get; set; } = default!;
     public string Color { get; set; } = default!;
