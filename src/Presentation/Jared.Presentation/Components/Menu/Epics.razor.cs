4721d922e5a9f60a6eee4f64dc9c30c2d2e26e37
Tomasz <trnosal@gmail.com>
2025-02-26 22:06:20 +0100

    Create new buttons

---
+++ src/Presentation/Jared.Presentation/Components/Menu/Epics.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace Jared.Presentation.Components.Menu;
+
+public partial class Epics
+{
+    private bool showUserMenu;
+    private bool showCreateDialog;
+
+    private string userMenuCssClass => showUserMenu ? "show-menu" : string.Empty;
+
+    private void toggleUserMenu()
+    {
+        showUserMenu = !showUserMenu;
+    }
+
+    private async Task hideUserMemu()
+    {
+        await Task.Delay(100);
+        showUserMenu = false;
+    }
+}
---
+++ src/Presentation/Jared.Presentation/Components/Menu/Projects.razor.cs
@@ -0,0 +1,20 @@
+﻿namespace Jared.Presentation.Components.Menu;
+
+public partial class Projects
+{
+    private bool showUserMenu;
+    private bool showCreateDialog;
+
+    private string userMenuCssClass => showUserMenu ? "show-menu" : string.Empty;
+
+    private void toggleUserMenu()
+    {
+        showUserMenu = !showUserMenu;
+    }
+
+    private async Task hideUserMemu()
+    {
+        await Task.Delay(100);
+        showUserMenu = false;
+    }
+}
