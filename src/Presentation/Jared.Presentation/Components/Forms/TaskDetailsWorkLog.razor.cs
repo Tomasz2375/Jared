c93c5e61118f804e0b0ab0c53e0ddad37ab54c0e
Tomasz <trnosal@gmail.com>
2025-03-01 00:46:06 +0100

    Add TaskDetailsWorkLog

---
+++ src/Presentation/Jared.Presentation/Components/Forms/TaskDetailsWorkLog.razor.cs
@@ -0,0 +1,10 @@
+﻿using Jared.Shared.Dtos.TaskDtos;
+using Microsoft.AspNetCore.Components;
+
+namespace Jared.Presentation.Components.Forms;
+
+public partial class TaskDetailsWorkLog
+{
+    [Parameter]
+    public TaskDetailsDto Dto { get; set; } = default!;
+}
