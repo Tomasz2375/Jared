5e6fbbfc12c17af100d552b8bcd80258e981ae74
Tomasz <trnosal@gmail.com>
2025-03-02 13:49:38 +0100

    Add delete buttons for worklogs

--- src/Presentation/Jared.Presentation/Components/Forms/TaskDetailsWorkLog.razor.cs
+++ src/Presentation/Jared.Presentation/Components/Forms/TaskDetailsWorkLog.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Jared.Shared.Dtos.TaskDtos;
+using Jared.Shared.Dtos.WorkLogDtos;
 using Microsoft.AspNetCore.Components;
 
 namespace Jared.Presentation.Components.Forms;
@@ -7,4 +8,30 @@ public partial class TaskDetailsWorkLog
 {
     [Parameter]
     public TaskDetailsDto Dto { get; set; } = default!;
+    private int userId;
+
+    protected override void OnInitialized()
+    {
+        userId = UserService.GetUserId();
+    }
+
+    private void deleteWorkLog(WorkLogListDto worklog)
+    {
+        worklog.Delete = true;
+
+        calculeteWorkTime();
+    }
+
+    private void restoreWorkLog(WorkLogListDto worklog)
+    {
+        worklog.Delete = false;
+
+        calculeteWorkTime();
+    }
+
+    private void calculeteWorkTime()
+    {
+        Dto.TotalWorkTime = new TimeSpan(
+            Dto.WorkLogs.Where(x => !x.Delete).Sum(x => x.Time.Ticks));
+    }
 }
