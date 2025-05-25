247c466ad59cc3d5b30000937991fdb3517c291f
Tomasz <trnosal@gmail.com>
2025-05-25 10:40:23 +0200

    Create notification component

---
+++ src/Presentation/Jared.Presentation/Components/Dialog/Notification.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Jared.Presentation.Components.Dialog;
+
+public partial class Notification
+{
+    protected override void OnInitialized()
+    {
+        NotificationService.OnChange += handleChange;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            NotificationService.OnChange -= handleChange;
+        }
+    }
+
+    private async void handleChange()
+    {
+        await InvokeAsync(StateHasChanged);
+    }
+
+    ~Notification()
+    {
+        Dispose(false);
+    }
+}
