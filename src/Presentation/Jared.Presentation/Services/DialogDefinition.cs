dbf713f5822845f383e983e334ee34041af497f5
Tomasz <trnosal@gmail.com>
2025-06-01 09:32:00 +0200

    Add DialogBase and DialogDefinition

---
+++ src/Presentation/Jared.Presentation/Services/DialogDefinition.cs
@@ -0,0 +1,7 @@
+﻿namespace Jared.Presentation.Services;
+
+public class DialogDefinition
+{
+    public Type ComponentType { get; set; } = default!;
+    public Dictionary<string, object> Parameters { get; set; } = new();
+}
