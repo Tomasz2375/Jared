c6740c7df9e19edc92ad25d4af01d09d47611b55
Tomasz <trnosal@gmail.com>
2025-06-01 09:32:35 +0200

    Create DialogContainer

---
+++ src/Presentation/Jared.Presentation/Components/Dialog/DialogContainer.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Jared.Presentation.Components.Dialog;
+
+public partial class DialogContainer
+{
+    protected override void OnInitialized()
+    {
+        DialogService.OnDialogsUpdated += StateHasChanged;
+    }
+
+    public void Dispose()
+    {
+        DialogService.OnDialogsUpdated -= StateHasChanged;
+        GC.SuppressFinalize(this);
+    }
+}
