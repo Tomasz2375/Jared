2c3a9f1efb7ebaa370e9f29647292ba137eed1a5
Tomasz <trnosal@gmail.com>
2025-12-23 21:25:08 +0100

    Create RefreshToken table

---
+++ src/api/Jared.Infrastructure/ModelConfiguration/UserRefreshTokenConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Jared.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Jared.Infrastructure.ModelConfiguration;
+
+public class UserRefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+{
+    public void Configure(EntityTypeBuilder<RefreshToken> builder)
+    {
+        builder.Property(x => x.CreatedAtUtc).HasDefaultValueSql("GETUTCDATE()");
+        builder.Property(x => x.Token).HasMaxLength(512);
+    }
+}
