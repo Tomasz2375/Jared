db921411110ce03f92362c926f3944ab976569c1
Tomasz <trnosal@gmail.com>
2025-12-27 13:34:34 +0100

    Create estimated end total work time columns minutes

---
+++ src/api/Jared.Infrastructure/Migrations/20251227123026_JAR-64-UpdateTaskEntity.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jared.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class JAR64UpdateTaskEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EstimatedTimeMinutes",
+                table: "Task",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TotalWorkTimeMinutes",
+                table: "Task",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.Sql(@"
+                UPDATE Task
+                SET EstimatedTimeMinutes = DATEDIFF(MINUTE, 0, EstimatedTime),
+                    TotalWorkTimeMinutes = DATEDIFF(MINUTE, 0, TotalWorkTime)
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EstimatedTimeMinutes",
+                table: "Task");
+
+            migrationBuilder.DropColumn(
+                name: "TotalWorkTimeMinutes",
+                table: "Task");
+        }
+    }
+}
