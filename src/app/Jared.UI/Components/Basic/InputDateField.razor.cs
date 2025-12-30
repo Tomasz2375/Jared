c49daca3ed97a1d0e82728bfd855f79fe05ca538
Tomasz <trnosal@gmail.com>
2025-12-30 13:08:47 +0100

    Update InputDateField

--- src/app/Jared.UI/Components/Basic/InputDateField.razor.cs
+++ src/app/Jared.UI/Components/Basic/InputDateField.razor.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System.Linq.Expressions;
 
 namespace Jared.UI.Components.Basic;
 
 public partial class InputDateField
 {
     [Parameter]
-    public Expression<Func<DateTime?>> ValidationFor { get; set; } = default!;
+    [EditorRequired]
+    public string Id { get; set; } = default!;
+
     [Parameter]
-    public string? Id { get; set; }
-    [Parameter]
-    public string? Label { get; set; }
+    [EditorRequired]
+    public string Placeholder { get; set; } = default!;
 
     protected override bool TryParseValueFromString(string? value, out DateTime? result, out string validationErrorMessage)
     {
